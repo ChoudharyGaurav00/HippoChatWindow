703cb227e29ad06c992ffa6bc8454dff2bc2eba3
ChoudharyGaurav00 <gauravchoudhary1047@gmail.com>
2023-02-02 18:07:15 +0530

    show time with message

--- src/app/chatwindowheader/chatwindowheader.component.ts
+++ src/app/chatwindowheader/chatwindowheader.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { ChatdataService } from '../chatdata.service';
 @Component({
   selector: 'app-chatwindowheader',
   templateUrl: './chatwindowheader.component.html',
   styleUrls: ['./chatwindowheader.component.css']
 })
-export class ChatwindowheaderComponent {
-
+export class ChatwindowheaderComponent implements OnInit {
+  
+  public id:number=-2;
   constructor (public chatService:ChatdataService){}
   initiateChat(name:string){
       // console.log(name);
       this.chatService.storeData(name);
+  }
+  ngOnInit() {
+    this.id=this.chatService.chatid;
   }
 }
